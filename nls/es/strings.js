cdd7f85bfe7b11c8b9aba138f371689d58ca8650
RMM <kake_757@outlook.es>
2019-04-06 12:06:43 +0200

    Tema Ifach
    Tema WAB para representar un modelo 3D

---
+++ nls/es/strings.js
@@ -0,0 +1,5 @@
+define({
+  "_themeLabel": "Ifach3DTheme",
+  "_layout_default": "Diseño predeterminado",
+  "_layout_right": "Diseño derecho"
+});
---
+++ nls/strings.js
@@ -0,0 +1,8 @@
+define({
+  root: ({
+    _themeLabel: "Ifach3DTheme",
+    _layout_default: "Default layout",
+    _layout_right: "Right layout"
+  }),
+  "es": 1,
+});
